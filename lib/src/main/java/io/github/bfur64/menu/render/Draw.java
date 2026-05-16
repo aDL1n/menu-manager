6fb9c388ac3b822e447c5131a2c08fe0511c32b6
BFUR64 <iterranceevan@gmail.com>
2026-05-16 09:48:13 +0800

    Removed hardcoded rendering
    Removed all mentions of hardcoded rendering, in favor of the new `tetrue-terminal-1.2.3` library

--- lib/src/main/java/io/github/bfur64/menu/render/Draw.java
+++
@@ -1,7 +0,0 @@
-package io.github.bfur64.menu.render;
-
-public record Draw(int x, int y, String out, Style fg, Style bg) {
-    public Draw(int x, int y, String out) {
-        this(x, y, out, null, null);
-    }
-}
--- lib/src/main/java/io/github/bfur64/menu/render/Style.java
+++
@@ -1,7 +0,0 @@
-package io.github.bfur64.menu.render;
-
-public record Style(int r, int g, int b) {
-    public Style() {
-        this(0, 0, 0);
-    }
-}
