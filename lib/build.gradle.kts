c481bccc1ea5b3dbaaf6451befec13ddf0cbb636
BFUR64 <iterranceevan@gmail.com>
2026-05-17 16:24:23 +0800

    Feat: 0.2.3

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@
  */
 
 group = "io.github.bfur64"
-version = "0.2.2"
+version = "0.2.3"
 
 plugins {
     // Apply the java-library plugin for API and implementation separation.
@@ -47,7 +47,7 @@ java {
 }
 
 application {
-    mainClass ="examples.StressTest";
+    mainClass ="examples.DemoEditable";
 }
 
 mavenPublishing {
