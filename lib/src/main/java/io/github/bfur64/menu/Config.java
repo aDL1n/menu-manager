3a259d9d81581d39a6ab2daa0d12f38cc6dbd400
BFUR64 <iterranceevan@gmail.com>
2026-05-26 06:10:03 +0800

    fix: Proper try close resource management in Config
    It didn't exist before this. For some reason my IDE didn't warn me? Perhaps it was accidentally ignored. It is now wrapped in a try catch except pattern, with a default handler for the fields and sysouting for failed messages. Should replace in the future with better logging.

--- lib/src/main/java/io/github/bfur64/menu/Config.java
+++ lib/src/main/java/io/github/bfur64/menu/Config.java
@@ -4,18 +4,29 @@ import org.jspecify.annotations.NullMarked;
 import tools.jackson.databind.JsonNode;
 import tools.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 
 @NullMarked
 class Config {
     public static final String VERSION;
 
     static {
-        InputStream is = Config.class.getResourceAsStream("/io/github/bfur64/menu/settings.json");
+        String version;
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode jsonNode = objectMapper.readTree(is);
+        try (InputStream is = Config.class.getResourceAsStream("/io/github/bfur64/menu/settings.json")) {
 
-        VERSION = jsonNode.get("version").asString();
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(is);
+
+            version = jsonNode.get("version").asString();
+        }
+        catch (IOException e) {
+            version = "null";
+            System.out.println("Failed: " + e.getMessage() + Arrays.toString(e.getStackTrace()));
+        }
+
+        VERSION = version;
     }
 }
