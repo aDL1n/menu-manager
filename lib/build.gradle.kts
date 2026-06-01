a18e35db1973770893610dc21226d3ede61b5389
BFUR64 <iterranceevan@gmail.com>
2026-06-01 20:08:24 +0800

    chore: Updated to Tetrue Terminal v2.4.2

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
     implementation(libs.guava)
 
     // Rendering Pipeline
-    implementation("io.github.bfur64:tetrue-terminal:2.4.0")
+    implementation("io.github.bfur64:tetrue-terminal:2.4.2")
 
     // JSON Reader
     implementation("tools.jackson.core:jackson-databind:3.1.3")
